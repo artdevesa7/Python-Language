b6d5745f6229d38f074b7efe2a53d572213624d4
artdevesa7 <63007186+artdevesa7@users.noreply.github.com>
2020-08-04 23:24:51 -0400

    if __name__ == __main__

---
+++ First_module.py
@@ -0,0 +1,6 @@
+import Second_module
+
+print("First module name is: {}".format(__name__))
+
+if __name__ == "__main__":
+    print(__name__)
---
+++ Second_module.py
@@ -0,0 +1,5 @@
+import First_module
+
+print("i'm second module")
+print(__name__)
+print("Second module name is: {}".format(__name__))
